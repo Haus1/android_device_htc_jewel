1ef204ce2b333a200c5d5faa2904958c86b0940e
Jon <haus.xda@gmail.com>
2014-02-02 15:18:19 -0500

    jewel: tweaks
    remove all but en_us locale and use ccache
    
    Change-Id: I4e02fce9e5859590e9ceb1dbfec93b97bd0b90a9

--- jewel.mk
+++ jewel.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+USE_CCACHE := 1
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # common S4 configs
@@ -103,7 +105,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-PRODUCT_LOCALES += en_US
+PRODUCT_LOCALES := en_US
 
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/jewel/jewel-vendor.mk)
